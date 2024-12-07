fac0db965641367754389ab490001252d1a4fe93
Rauf Shimarov <shimarov00@gmail.com>
2024-12-07 15:51:27 -0800

    Add params to triggering flow

--- flows/triggering_event_flow.py
+++ flows/triggering_event_flow.py
@@ -2,16 +2,16 @@ from prefect import flow, task, get_run_logger
 from prefect.events import emit_event
 
 @task
-def trigger_event(event):
+def trigger_event(event, param1, param2):
     logger = get_run_logger()
     logger.info(f"Emitting event {event}...")
 
-    emit_event(event=event, resource={"prefect.resource.id": "my-triggering-resource-id"}, payload={"param1": "someParamValue", "param2": "someOtherParamValue"})
+    emit_event(event=event, resource={"prefect.resource.id": "my-triggering-resource-id"}, payload={"param1": param1, "param2": param2})
 
 
 @flow(name="Triggering Event Flow")
-def triggering_event_flow(event="some.test.event"):
-    trigger_event(event)
+def triggering_event_flow(event="some.test.event", param1="defaultParam1", param2="defaultParam2"):
+    trigger_event(event, param1, param2)
 
 
 if __name__ == "__main__":
