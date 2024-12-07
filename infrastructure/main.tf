ef9fb3241f4713088d6c976f931cf8b49af53625
Rauf Shimarov <shimarov00@gmail.com>
2024-12-07 03:56:16 -0800

    Fix workflows

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -31,7 +31,8 @@ resource "google_project_iam_member" "cicd-sa" {
     "roles/iam.workloadIdentityPoolAdmin",
     "roles/resourcemanager.projectIamAdmin",
     "roles/secretmanager.admin",
-    "roles/artifactregistry.reader"
+    "roles/artifactregistry.admin",
+    "roles/run.admin"
   ])
   role    = each.value
   project = var.gcp_project_id
